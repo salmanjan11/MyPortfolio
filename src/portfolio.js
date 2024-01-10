db784c3de2ca047f1f13bd468708d25fad5481e0
Salman Jan <salmanjan1234@gmail.com>
2024-01-10 23:18:43 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -343,7 +343,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+393517892015"
+  number: "+393517892015",
   email_address: "salmanjan.ipb@gmail.com"
 };
 
